c87e8a5a1165772a77bf5619330e06fe238148a7
Max Verevkin <maxxverrr@gmail.com>
2026-04-07 09:37:21 +0300

    ir_tree/dump: refactor after the transition to Visitor trait

--- src/ir_tree/dump.rs
+++ src/ir_tree/dump.rs
@@ -5,162 +5,161 @@ use crate::ir_tree::visit::ExprVisitor;
 use super::*;
 
 pub fn dump(module: &Module) -> String {
-    let mut output = String::new();
+    let mut writer = Writer {
+        output: String::new(),
+        module,
+        indent_level: 1,
+    };
 
     for function in module.functions.values() {
-        dump_function_desc(&mut output, function, module);
+        writer.dump_function_desc(function);
         if let Some(body) = &function.body {
-            dump_expr(&mut output, body, module, 1);
+            writer.visit_expr(body);
         }
-        output.push('\n');
+        writer.output.push('\n');
     }
 
-    output
+    writer.output
 }
 
-fn dump_function_desc(output: &mut String, function: &Function, module: &Module) {
-    output.push_str("fn ");
-    output.push_str(&function.name.value);
-    output.push('(');
-    for (arg_i, (arg_name, arg_ty)) in function.args.iter().enumerate() {
-        output.push_str(arg_name);
-        output.push_str(": ");
-        dump_type(output, *arg_ty, module);
-        if arg_i + 1 != function.args.len() || function.is_variadic {
-            output.push_str(", ");
-        }
-    }
-    if function.is_variadic {
-        output.push_str("...");
-    }
-    output.push_str(") -> ");
-    dump_type(output, function.return_ty, module);
-    output.push('\n');
+struct Writer<'a> {
+    output: String,
+    module: &'a Module,
+    indent_level: u32,
 }
 
-fn dump_type(output: &mut String, ty: Type, module: &Module) {
-    match ty {
-        Type::Never => output.push('!'),
-        Type::Unit => output.push_str("unit"),
-        Type::Bool => output.push_str("bool"),
-        Type::Int(int_type) => output.push_str(match int_type {
-            IntType::I8 => "i8",
-            IntType::U8 => "u8",
-            IntType::I32 => "i32",
-            IntType::U32 => "u32",
-            IntType::I64 => "i64",
-            IntType::U64 => "u64",
-        }),
-        Type::Struct(struct_id) => write!(output, "{struct_id:?}").unwrap(),
-        Type::Ptr { pointee: None } => output.push_str("ptr"),
-        Type::Ptr { pointee: Some(pointee) } => {
-            output.push('*');
-            dump_type(output, module.typesystem.get_type(pointee), module);
-        }
-        Type::Array { element, length } => {
-            output.push('[');
-            dump_type(output, module.typesystem.get_type(element), module);
-            write!(output, "; {length}]").unwrap();
-        }
-    }
-}
+impl ExprVisitor for Writer<'_> {
+    fn visit_expr(&mut self, expr: &Expr) {
+        self.indent();
 
-fn dump_expr(output: &mut String, expr: &Expr, module: &Module, indent_level: u32) {
-    indent(output, indent_level);
-
-    match &expr.kind {
-        ExprKind::Undefined => output.push_str("UNDEFINED"),
-        ExprKind::ConstUnit => output.push_str("UNIT"),
-        ExprKind::ConstNumber(num) => write!(output, "CONST_NUMBER({num})").unwrap(),
-        ExprKind::ConstString(str) => write!(output, "CONST_STRING({str:?})").unwrap(),
-        ExprKind::ConstBool(bool) => write!(output, "CONST_BOOL({bool})").unwrap(),
-        ExprKind::Load(_) => output.push_str("LOAD"),
-        ExprKind::Field(_, field) => write!(output, "FIELD_ACCESS({field})").unwrap(),
-        ExprKind::ArrayElement(_, _) => output.push_str("ARRAY_ELEMENT"),
-        ExprKind::Store(_, _) => output.push_str("STORE"),
-        ExprKind::GetPointer(_) => output.push_str("GET_POINTER"),
-        ExprKind::Argument(arg) => write!(output, "ARGUMENT({arg:?})").unwrap(),
-        ExprKind::Block(_) => output.push_str("BLOCK"),
-        ExprKind::Return(_) => output.push_str("RETURN"),
-        ExprKind::Break(loop_id, _) => write!(output, "BREAK({loop_id:?})").unwrap(),
-        ExprKind::Arithmetic(op, _, _) => write!(output, "ARITHMETIC({op:?})").unwrap(),
-        ExprKind::Cmp(op, _, _) => write!(output, "CMP({op:?})").unwrap(),
-        ExprKind::If { .. } => output.push_str("IF"),
-        ExprKind::Loop(loop_id, _) => write!(output, "LOOP({loop_id:?})").unwrap(),
-        ExprKind::ArrayInitializer(_) => output.push_str("ARRAY_INITIALIZER"),
-        ExprKind::StructInitializer(_) => output.push_str("STRUCT_INITIALIZER"),
-        ExprKind::FunctionCall(function_id, _) => write!(
-            output,
-            "FUNCTION_CALL({:?})",
-            module.functions.get(function_id).unwrap().name.value
-        )
-        .unwrap(),
-        ExprKind::Cast(_) => output.push_str("CAST"),
-        ExprKind::Not(_) => output.push_str("NOT"),
-    }
-
-    output.push_str(" TYPE=");
-    dump_type(output, expr.ty, module);
-    output.push('\n');
-
-    if let ExprKind::Block(bexpr) = &expr.kind {
-        for (var_id, var_ty) in &bexpr.variables {
-            indent(output, indent_level + 1);
-            write!(output, "DECLARE {var_id:?} : ").unwrap();
-            dump_type(output, *var_ty, module);
-            output.push('\n');
-        }
-    }
-
-    expr.visit_children(&mut Visitor {
-        output,
-        module,
-        indent_level: indent_level + 1,
-    });
-
-    struct Visitor<'a> {
-        output: &'a mut String,
-        module: &'a Module,
-        indent_level: u32,
-    }
-
-    impl ExprVisitor for Visitor<'_> {
-        fn visit_expr(&mut self, expr: &Expr) {
-            dump_expr(self.output, expr, self.module, self.indent_level);
+        match &expr.kind {
+            ExprKind::Undefined => self.output.push_str("UNDEFINED"),
+            ExprKind::ConstUnit => self.output.push_str("UNIT"),
+            ExprKind::ConstNumber(num) => write!(self.output, "CONST_NUMBER({num})").unwrap(),
+            ExprKind::ConstString(str) => write!(self.output, "CONST_STRING({str:?})").unwrap(),
+            ExprKind::ConstBool(bool) => write!(self.output, "CONST_BOOL({bool})").unwrap(),
+            ExprKind::Load(_) => self.output.push_str("LOAD"),
+            ExprKind::Field(_, field) => write!(self.output, "FIELD_ACCESS({field})").unwrap(),
+            ExprKind::ArrayElement(_, _) => self.output.push_str("ARRAY_ELEMENT"),
+            ExprKind::Store(_, _) => self.output.push_str("STORE"),
+            ExprKind::GetPointer(_) => self.output.push_str("GET_POINTER"),
+            ExprKind::Argument(arg) => write!(self.output, "ARGUMENT({arg:?})").unwrap(),
+            ExprKind::Block(_) => self.output.push_str("BLOCK"),
+            ExprKind::Return(_) => self.output.push_str("RETURN"),
+            ExprKind::Break(loop_id, _) => write!(self.output, "BREAK({loop_id:?})").unwrap(),
+            ExprKind::Arithmetic(op, _, _) => write!(self.output, "ARITHMETIC({op:?})").unwrap(),
+            ExprKind::Cmp(op, _, _) => write!(self.output, "CMP({op:?})").unwrap(),
+            ExprKind::If { .. } => self.output.push_str("IF"),
+            ExprKind::Loop(loop_id, _) => write!(self.output, "LOOP({loop_id:?})").unwrap(),
+            ExprKind::ArrayInitializer(_) => self.output.push_str("ARRAY_INITIALIZER"),
+            ExprKind::StructInitializer(_) => self.output.push_str("STRUCT_INITIALIZER"),
+            ExprKind::FunctionCall(function_id, _) => write!(
+                self.output,
+                "FUNCTION_CALL({:?})",
+                self.module.functions.get(function_id).unwrap().name.value
+            )
+            .unwrap(),
+            ExprKind::Cast(_) => self.output.push_str("CAST"),
+            ExprKind::Not(_) => self.output.push_str("NOT"),
         }
 
-        fn visit_place(&mut self, place: &Place) {
-            indent(self.output, self.indent_level);
+        self.output.push_str(" TYPE=");
+        self.dump_type(expr.ty);
+        self.output.push('\n');
 
-            match &place.kind {
-                PlaceKind::Dereference(_) => self.output.push_str("DEREFERENCE"),
-                PlaceKind::Variable(variable_id) => write!(self.output, "VARIABLE({variable_id:?})").unwrap(),
-                PlaceKind::Field(_, field) => write!(self.output, "FIELD({field:?})").unwrap(),
-                PlaceKind::ArrayElement(_, _) => self.output.push_str("ARRAY_ELEMENT"),
+        self.indent_level += 1;
+        if let ExprKind::Block(bexpr) = &expr.kind {
+            for (var_id, var_ty) in &bexpr.variables {
+                self.indent();
+                write!(self.output, "DECLARE {var_id:?} : ").unwrap();
+                self.dump_type(*var_ty);
+                self.output.push('\n');
             }
-
-            self.output.push_str(" [PLACE] TYPE=");
-            dump_type(self.output, place.ty, self.module);
-            self.output.push('\n');
-
-            self.indent_level += 1;
-            place.visit_children(self);
-            self.indent_level -= 1;
         }
+        expr.visit_children(self);
+        self.indent_level -= 1;
+    }
+
+    fn visit_place(&mut self, place: &Place) {
+        self.indent();
+
+        match &place.kind {
+            PlaceKind::Dereference(_) => self.output.push_str("DEREFERENCE"),
+            PlaceKind::Variable(variable_id) => write!(self.output, "VARIABLE({variable_id:?})").unwrap(),
+            PlaceKind::Field(_, field) => write!(self.output, "FIELD({field:?})").unwrap(),
+            PlaceKind::ArrayElement(_, _) => self.output.push_str("ARRAY_ELEMENT"),
+        }
+
+        self.output.push_str(" [PLACE] TYPE=");
+        self.dump_type(place.ty);
+        self.output.push('\n');
+
+        self.indent_level += 1;
+        place.visit_children(self);
+        self.indent_level -= 1;
     }
 }
 
-fn indent(output: &mut String, indent: u32) {
-    if indent > 0 {
-        output.push(' ');
-        output.push(' ');
-        output.push(' ');
-        output.push(' ');
-        for _ in 1..indent {
-            output.push('|');
-            output.push(' ');
-            output.push(' ');
-            output.push(' ');
+impl Writer<'_> {
+    fn indent(&mut self) {
+        if self.indent_level > 0 {
+            self.output.push(' ');
+            self.output.push(' ');
+            self.output.push(' ');
+            self.output.push(' ');
+            for _ in 1..self.indent_level {
+                self.output.push('|');
+                self.output.push(' ');
+                self.output.push(' ');
+                self.output.push(' ');
+            }
         }
+    }
+
+    fn dump_type(&mut self, ty: Type) {
+        match ty {
+            Type::Never => self.output.push('!'),
+            Type::Unit => self.output.push_str("unit"),
+            Type::Bool => self.output.push_str("bool"),
+            Type::Int(int_type) => self.output.push_str(match int_type {
+                IntType::I8 => "i8",
+                IntType::U8 => "u8",
+                IntType::I32 => "i32",
+                IntType::U32 => "u32",
+                IntType::I64 => "i64",
+                IntType::U64 => "u64",
+            }),
+            Type::Struct(struct_id) => write!(self.output, "{struct_id:?}").unwrap(),
+            Type::Ptr { pointee: None } => self.output.push_str("ptr"),
+            Type::Ptr { pointee: Some(pointee) } => {
+                self.output.push('*');
+                self.dump_type(self.module.typesystem.get_type(pointee));
+            }
+            Type::Array { element, length } => {
+                self.output.push('[');
+                self.dump_type(self.module.typesystem.get_type(element));
+                write!(self.output, "; {length}]").unwrap();
+            }
+        }
+    }
+
+    fn dump_function_desc(&mut self, function: &Function) {
+        self.output.push_str("fn ");
+        self.output.push_str(&function.name.value);
+        self.output.push('(');
+        for (arg_i, (arg_name, arg_ty)) in function.args.iter().enumerate() {
+            self.output.push_str(arg_name);
+            self.output.push_str(": ");
+            self.dump_type(*arg_ty);
+            if arg_i + 1 != function.args.len() || function.is_variadic {
+                self.output.push_str(", ");
+            }
+        }
+        if function.is_variadic {
+            self.output.push_str("...");
+        }
+        self.output.push_str(") -> ");
+        self.dump_type(function.return_ty);
+        self.output.push('\n');
     }
 }
